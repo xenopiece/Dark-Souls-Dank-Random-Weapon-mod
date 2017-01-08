a7386a0f0bc10fd6f29ba3b67e9d5dcd9e679a58
Niller303 <seje.niels@hotmail.com>
2017-01-08 11:26:16 +0100

    Fixed size

--- RandomWeaponMod/RandomWeaponMod.cpp
+++ RandomWeaponMod/RandomWeaponMod.cpp
@@ -114,7 +114,7 @@ int main()
 				HWND console = GetConsoleWindow();
 				RECT r;
 				GetWindowRect(console, &r); //stores the console's current dimensions
-				MoveWindow(console, r.left, r.top, 400, 400, TRUE);
+				MoveWindow(console, r.left, r.top, 450, 420, TRUE);
 
 				// This wont actually work, nepSmug
 				EnableScrollBar(console, SB_BOTH, ESB_DISABLE_BOTH);
